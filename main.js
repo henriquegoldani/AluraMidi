f42544bd085940370e7f202d2bc9e027dbde434f
Henrique Goldani <henriquegoldani@gmail.com>
2022-12-14 08:46:27 -0300

    Criar Projeto

---
+++ main.js
@@ -0,0 +1,13 @@
+function tocaSomPom (){
+    document.querySelector('#som_tecla_pom').play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < 8) {
+    listaDeTeclas[contador].onclick = tocaSomPom;
+    contador = contador + 1;
+    console.log(contador);
+}
